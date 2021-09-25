d70503a9811444c8eee5d43c07a955e2586a4258
Mert <60737371+mrtuls1@users.noreply.github.com>
2021-09-25 10:28:59 +0300

    Update baseUrl.js

--- helpers/baseUrl.js
+++ helpers/baseUrl.js
@@ -1,4 +1,4 @@
 //Bu alanda projedeki url değişiklerinden etkilenilmemesi için dinamik bir url tanımlanmıştır
 //vercel sayesinde aşağıdaki urle projemizi göndermiş olduk
-const baseUrl = process.env.NODE_ENV === 'production' ? "e-ticaret-proje.vercel.app": "http://localhost:3000"
+const baseUrl = process.env.NODE_ENV === 'production' ? "eticaret2021.vercel.app": "http://localhost:3000"
 export default baseUrl
